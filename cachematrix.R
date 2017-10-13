748b5229128729b6e13824fee8932878a2a52e78
astaroth88 <swainshashwat@gmail.com>
2017-10-13 17:08:46 +0530

    Funtions added

--- cachematrix.R
+++ cachematrix.R
@@ -3,18 +3,19 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix())
-{
-inv<-NULL
-set<-function(y)
-{
-  x<<-y
-  inv<<-NULL
-}
-get<-function()x
-setinv<-function(inverse0)inv<<-inverse0
-getinv<-function()inv
-list(set=set,get=get,setinv=setinv,getinv=getinv)
+makeCacheMatrix <- function(x = matrix()) {
+  
+  inv<-NULL
+  set<-function(y)
+  {
+    x<<-y
+    inv<<-NULL
+  }
+  get<-function()x
+  setinv<-function(inverse0)inv<<-inverse0
+  getinv<-function()inv
+  list(set=set,get=get,setinv=setinv,getinv=getinv)
+
 }
 
 
@@ -22,14 +23,15 @@ list(set=set,get=get,setinv=setinv,getinv=getinv)
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        inv<-x$getinv()
-        if(!is.null(inv))
-        {
-          message("getting cached data")
-          return(inv)
-        }
-        data<-x$get()
-        inv<-solve(data,...)
-        x$setinv(inv)
-        inv
+  inv<-x$getinv()
+  if(!is.null(inv))
+  {
+    message("getting cached data")
+    return(inv)
+  }
+  data<-x$get()
+  inv<-solve(data,...)
+  x$setinv(inv)
+  inv
+  
 }
